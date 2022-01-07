8b83d176dd816af2ae9af5f248b73c0f0c00cc51
alex <alexvgc85@gmail.com>
2022-01-07 19:49:03 +0100

    GUI

---
+++ src/app/components/zeichenflaeche/zeichenflaeche.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zeichenflaeche',
+  templateUrl: './zeichenflaeche.component.html',
+  styleUrls: ['./zeichenflaeche.component.css']
+})
+
+/* 
+ * Author: Kern Handa
+ * https://kernhanda.github.io/tutorial-typescript-canvas-drawing/ 
+ * */
+export class ZeichenflaecheComponent implements OnInit {
+
+ ngOnInit(): void {
+     
+ }
+ constructor() {
+  var canvas = document.getElementById("zeichenflaeche");
+
+ }
+
+
+ 
+
+}
