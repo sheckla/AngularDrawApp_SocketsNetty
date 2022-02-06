d142f47dc87532c5fafd99aa5392e0183600c340
sheckla <DaNiEL11>
2022-02-06 17:54:09 +0100

    - configuration

--- src/app/socketio.service.ts
+++ src/app/socketio.service.ts
@@ -1,31 +1,83 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { io } from 'socket.io-client';
+import * as io from "socket.io-client";
+import { BehaviorSubject, Observable } from 'rxjs';
+import {enableProdMode} from '@angular/core';
+import * as express from "express";
+import * as socketio from "socket.io";
+
+enableProdMode();
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketioService {
+  private socket;
+  public message$: BehaviorSubject<string> = new BehaviorSubject('');
+  private ws: WebSocket = new WebSocket('ws://localhost:9092/socket.io/?EIO=3&transport=websocket');
 
-  socket: any;
+  // Konstrukor wird mehrmals aufgerufen
+  constructor() {
+    console.log("constructor socket");
+    /*     this.socket = io("https://localhost:9092", {
+    transports: ["websocket"] // use WebSocket first, if available});
+  });
 
-  constructor() {   }
+  this.socket.on("connect_error", () => {  // revert to classic upgrade
+    this.socket.io.opts.transports = ["polling", "websocket"];
+  }); */
 
-  setupSocketConnection() {
-    this.socket = io(environment.SOCKET_ENDPOINT);
-    this.socket.emit('my message', 'Hello there from Angular');
-    this.socket.on('my broadcast', (data: string) => {
-      console.log(data);
-    });
+
+
+
+  /* this.ws.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+
+  }); */
+
+  /* event = new Event("message", {"bubbles":true, "cancelable":false});
+  dispatchEvent(event);
+
+  this.ws.addEventListener('open', (event) => {
+    this.ws.send('Hello Server!');
+  }); */
+
+}
+
+public sendMessage(message) {
+  //this.ws.send('message');
+  this.socket.emit("message", "Test");
+  console.log("sendMessage");
+  console.log(this.socket.connected);
+  this.socket.on("connect"), (event: any) => {
+    console.log("connect");
   }
+}
 
-  disconnect() {
-    if (this.socket) {
-      this.socket.disconnect();
-    }
-  }
+public getNewMessage = () => {
+  this.socket.on('message', (message) => {
+    this.message$.next(message);
+  })
+  return this.message$.asObservable();
+}
 
-  emitTest(val: number) {
-    this.socket.emit('my message', val);
-  }
+
+connected() {
+  console.log("socket_connected()")
+  this.socket = io.connect('http://localhost:9092'), { // Source CORS Error
+  widthCredentials: false,
+  transports: ["websocket"],
+  upgrade: false
+}
+this.socket.on('reconnect_attempt', () => {  this.socket.io.opts.transports = ['polling', 'websocket'];});
+
+this.socket.on("connect_error"), (event: any) => {
+  console.log("connect error");
+}
+
+this.socket.on("connect"), (event: any) => {
+  console.log("connect");
+}
+}
 }
